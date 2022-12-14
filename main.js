46d14bf44747092f2ed209b7a87029f6a1ed28c1
CREATOR BOT WA <98082226+LynnXzy@users.noreply.github.com>
2022-12-15 01:33:48 +0800

    Update main.js

--- main.js
+++ main.js
@@ -126,10 +126,10 @@ global.reloadHandler = function (restatConn) {
     conn.ev.off('creds.update', conn.credsUpdate)
   }
 
-  conn.welcome = 'Hai @user Selamat Datang\n\nDi Grup @subject\nKetik .intro untuk menampilkan card intro'
-  conn.bye = '@user Telah Meninggalkan Percakapan Grup.'
-  conn.spromote = '@user sekarang admin!'
-  conn.sdemote = '@user sekarang bukan admin!'
+  conn.welcome = 'Hai @user Selamat Datang\n\nDi Grup @subject\nKetik .intro untuk menampilkan card intro\n@desc'
+  conn.bye = 'Alhamdulillah Beban Out @user'
+  conn.spromote = '@user Anjay dijadiin admin ga tuh'
+  conn.sdemote = '@user Kasihan ga bsa jdi admin'
   conn.handler = handler.handler.bind(conn)
   conn.participantsUpdate = handler.participantsUpdate.bind(conn)
   conn.onDelete = handler.delete.bind(conn)
